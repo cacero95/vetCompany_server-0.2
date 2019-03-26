364edd5992e66c7ed49c6c8b3e0c4f25c03ccaa2
cacero95 <cacero95@hotmail.com>
2019-03-25 19:54:04 -0500

    second is better

--- config/config.js
+++ config/config.js
@@ -1,9 +1,13 @@
-/**
- * definimos el puerto por el que va ir
- * la aplicacion, si no estamos en desarrollo
- * entonces se asigna el puerto 3000
- */
+// ============
+// Puerto
+// ============
+
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============
+// Entorno
+// ============
 
 /**
  * se verifica si estamos local o en produccion osea online
@@ -14,24 +18,36 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // Db
 // ============
 
+let urlDb;
+
 if (process.env.NODE_ENV === 'dev') {
-    process.env.URLDB = 'mongodb://localhost:27017/vetcompany';
+    process.env.URLDB = 'mongodb://localhost:27017/cafe';
+} else {
+    process.env.URLDB = process.env.MONGO_URI;
 }
 
-// caducidad de los tokens
+// ========================
+// vencimiento del token
+// ========================
 
 process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30; // 30 dias
+// ===================================
+// seed || token secret auntenticacion
+// ===================================
 
+process.env.SEED = process.env.SEED || 'seed-de-la-aplicacion';
 
-/**
- * credenciales para acceder al api de twitter
- */
+// ===================================
+// google client id
+// ===================================
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '660910679754-7tgt311r4fd02nmubaj439bmo8ou36jl.apps.googleusercontent.com';
+
+// ===================================
+// Twitter credentials
+// ===================================
+
 process.env.twitter_key = process.env.twitter_key || 'hArIyoVy0lP8nOf2kV8xKeZ6I';
 process.env.twitter_secret = process.env.twitter_secret || 'iy5R0dfr7ngLz4YcXc6ZTT19lAbxay1OkbKFbLcO30pwxA6nyd';
 process.env.twitter_token_key = process.env.twitter_token_key || '2355469728-mHnWjplpYnrLf2SxB5hnuAPQ8ezU6gIdV3nVcpi';
 process.env.twitter_token_secret = process.env.twitter_token_secret || '6YZsUnwiSvUASMAE3SXS5oZcm2z89ej1VnEFzqS8BNFzz';
-
-/**
- * google credentials
- */
-process.env.CLIENT_ID = process.env.CLIENT_ID || '660910679754-7tgt311r4fd02nmubaj439bmo8ou36jl.apps.googleusercontent.com';
